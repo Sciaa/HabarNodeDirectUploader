76903d257fe098f625dec46810caa5bb4869f310
Harold Isaac Barrios <harold.barrios@outlook.com>
2017-07-08 17:25:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get('/sign-s3', (req, res) => {
     }
     const returnData = {
       signedRequest: data,
-      url: 'https://${S3_BUCKET}.s3.amazonaws.com/${fileName}'
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
     };
     res.write(JSON.stringify(returnData));
     res.end();
